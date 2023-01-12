3c4640b37977f679ead0988706504747ca790f8a
damir <yespeace@bk.ru>
2023-01-12 14:38:10 +0600

    Initial Commit

---
+++ iOS Swift GraphqlUITests/iOS_Swift_GraphqlUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  iOS_Swift_GraphqlUITestsLaunchTests.swift
+//  iOS Swift GraphqlUITests
+//
+//  Created by Damir Aushenov on 12/1/23.
+//
+
+import XCTest
+
+class iOS_Swift_GraphqlUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
